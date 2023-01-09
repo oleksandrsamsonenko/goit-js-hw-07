4defd797782f7266485f2acf1bdb119800e6676d
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-10 00:23:31 +0200

    second part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = document.querySelector(`.gallery`);
+
+galleryItems.forEach((element) => {
+  galleryMarkup.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`
+  );
+});
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+gallery.on("show.simplelightbox", function () {});
